b2145dae4a0e8fae2ab456d5ba4c97262759c9b5
Tatsuro Muto <tomuto0221@gmail.com>
2020-07-10 16:46:59 +0900

    [finish]カリキュラムrubocop実行

--- Gemfile
+++ Gemfile
@@ -48,6 +48,9 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+
+  # rubocop追加
+  gem 'rubocop-airbnb'
 end
 
 group :test do
@@ -61,5 +64,5 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-#pry-byebug追加
+# pry-byebug追加
 gem 'pry-byebug'
--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -62,13 +62,14 @@ class MemosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_memo
-      @memo = Memo.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def memo_params
-      params.require(:memo).permit(:title, :comment)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_memo
+    @memo = Memo.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def memo_params
+    params.require(:memo).permit(:title, :comment)
+  end
 end
